b307f012a004e818a1780c3a0ca1e28785ed8814
Muhammad Waqas <mwaqassabirofficial@gmail.com>
2023-12-15 11:56:23 +0500

    added methods

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,32 @@
-export default  {
-	of: (str: string) => {
-		return {
-			repeat: (times: number) => str.repeat(Number(times)),
-			// Add other string manipulation methods as needed
-		};
-	},
-}
+const str = {
+  // Fluent Strings
+  of: (str: string) => {
+    return {
+      repeat: (times: number) => str.repeat(Number(times)),
+      // Add other string manipulation methods as needed
+    };
+  },
 
+  //   Strings
+  lower: (str: string) => {
+    return str.toLowerCase();
+  },
+  upper: (str: string) => {
+    return str.toUpperCase();
+  },
+  capitalize: (str: string) => {
+    return str.replace(/(^\w{1})|(\s+\w{1})/g, (letter) =>
+      letter.toUpperCase()
+    );
+  },
+};
+
+// individual
+const lower = (str: string) => {
+  return str.toLowerCase();
+};
+const upper = (str: string) => {
+  return str.toUpperCase();
+};
+
+export { str, lower, upper };
